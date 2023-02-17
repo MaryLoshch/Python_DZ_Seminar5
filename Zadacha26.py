0e5bf4a9f3cffedac5eea781d4adb749427916c6
мария <oops.mari@mail.ru>
2023-02-17 08:43:05 +0300

    26

--- Zadacha26.py
+++ Zadacha26.py
@@ -2,15 +2,15 @@
 # два числа A и B, и возводит число А в целую степень B с
 # помощью рекурсии.
 
-a = int (input("Введите число A: "))
-b = int (input("Введите число B: "))
+a = int(input("Введите число A: "))
+b = int(input("Введите число B: "))
 
 
-def result (a, b):
+def result(a, b):
     if (b == 1):
         return (a)
     elif (b != 1):
-        return (a * (result (a, b - 1)))
+        return (a * (result(a, b - 1)))
 
-print ('Число A в степени B равно - ',
-result (a, b))
+
+print(f'Число A в степени B равно - {result(a,b)}')
